77e5a7bafd82a36c3c0efb1aa70e40656452e588
David R <99555747+GitExcited@users.noreply.github.com>
2025-12-01 17:21:48 -0500

    Cleaned code

--- src/day_01.py
+++ src/day_01.py
@@ -34,27 +34,19 @@ class Dial:
         for move in moves:
             direction = move[0]  # L or R
             amount = move[1]
-            print(f"*Movement: Direction:{direction} by {amount}")
             if direction == 'L':
                 self.move_left(amount)
             else:
-
                 self.move_right(amount)
-
-
-
-
 
 class SpecialDial (Dial):
     def __init__(self):
         super().__init__()
 
     def passes_by_zero_right(self, start, moves):
-        print(f"Dial moved right from{start} to {(start + moves) % self.size}")
 
         # Special case: starting at 0 and moving right
         if start == 0:
-
             repetitions = moves // self.size
             return repetitions
 
@@ -70,61 +62,39 @@ class SpecialDial (Dial):
         return repetitions
 
     def passes_by_zero_left(self, start, moves):
-        print(f"Dial moved left  from{start} to {(start - moves) % self.size}")
 
-        # Special case: starting at 0 and moving left
         if start == 0:
             repetitions = moves // self.size
             return repetitions
 
-        # Normal case: starting at position > 0
         if start > moves:
             return 0
 
-        # We land on or cross 0 at least once
         remaining = moves - start
         repetitions = 1 + (remaining // self.size)
         return repetitions
+
     def move_left(self,moves):
+
         passed_zeros = self.passes_by_zero_left(self.pointer, moves)
         self.zeros += passed_zeros
-        print("|")
-        print(f"l-----> Encountered {passed_zeros }zeros")
         self.pointer = (self.pointer - moves) % self.size
 
     def move_right(self,moves):
-        print("moving right")
-        passed_zeros= self.passes_by_zero_right(self.pointer,moves)
-        self.zeros += passed_zeros
-        print("|")
-        print(f"l-----> Encountered {passed_zeros}zeros")
-        self.pointer = (self.pointer +moves)% self.size
-        print(f"Final pointer at {self.pointer}")
 
-# movements = [
-#     ('L',68),
-#     ('L',30),
-#     ('R',48),
-#     ('L',5),
-#   ('R',60),
-# ('L',55),
-# ('L',1),
-# ('L',99),
-# ('R',14),
-#     ('L',82)
-# ]
+        self.zeros += self.passes_by_zero_right(self.pointer,moves)
+        self.pointer = (self.pointer +moves)% self.size
 
 if __name__ == "__main__":
 
-    # Move dial according to input
-
+    # PART 1
     dial = Dial()
-    # dial.move_dial(movements)
-    # Total zeros:
+    dial.move_dial(movements)
     print("Part 1 Answer ⭐")
     print(dial.zeros)
     print("================")
 
+    #PART 2
     dial = SpecialDial()
     dial.move_dial(movements)
     print( "Part 2 Answer ⭐")
