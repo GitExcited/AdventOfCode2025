420e7578a5baa400fb6c4cb889b9dbaedfb8e197
David R <99555747+GitExcited@users.noreply.github.com>
2025-12-05 14:08:47 -0500

    day 4- Star 2 DONE- David

--- src/day_04.py
+++ src/day_04.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 INPUT_PATH  = Path("../inputs/day_04.txt")
-TOTAL_LINES = 139 # From the .txt file, could've also written code to count them
+TOTAL_LINES = 0
 with open(INPUT_PATH) as file:
     grid = [[el for el in line.rstrip()] for line in file]
     blank = ['.' for i in range(len(grid[0])) ]
@@ -12,27 +12,44 @@ with open(INPUT_PATH) as file:
         line.insert(0,'.')
         line.append('.')
     #Basically transforming the original grid with a padding of '.' all around
-
+for row in grid:
+    TOTAL_LINES+=1
 sum = 0
-for i in range(1,TOTAL_LINES-1):
-    print(f"i{i}")
-    for j in range(1,len(grid[i])-1):
-        print(f"j{j}")
-        current= grid[i][j]
-        if current!= '@': continue
-        count = 0 #Number of rolls around
-        top = i-1
-        bottom = i+1
-        left = j-1
-        right = j+1
-        coordinates_around = [grid[top][left], grid[top][j], grid[top][right],
-                              grid[i][left],              grid[i][right],
-                              grid[bottom][left],grid[bottom][j], grid[bottom][right]]
 
-        print(coordinates_around)
-        for coordinate in coordinates_around:
-            if coordinate =='@':count+=1
-        if count<4:
-            sum +=1
+changed = False
+first_time = True
+
+# while changed or first_time:
+#     if first_time:
+#         first_time = False
+#     changed = False
+i = 1
+j = 1
+while i < (TOTAL_LINES-1):
+    while j< (len(grid[i])-1):
+        current= grid[i][j]
+        if current== '@':
+            count = 0 #Number of rolls around
+            top = i-1
+            bottom = i+1
+            left = j-1
+            right = j+1
+            coordinates_around = [grid[top][left], grid[top][j], grid[top][right],
+                                  grid[i][left],              grid[i][right],
+                                  grid[bottom][left],grid[bottom][j], grid[bottom][right]]
+            for coordinate in coordinates_around:
+                if coordinate =='@':count+=1
+            if count<4:
+                sum +=1
+                grid[i][j]= "."
+                print(f"Found one that got switched, we used to be at {i},{j}")
+                i = top
+                j = left-1 #Move pointer to top left and retry
+                print(f"Now we at {i},{j}")
+
+
+        j+=1
+    i+=1
+    j=1
 
 print(sum)
