3299937c16ddae555792d5254b3e32aae700eb30
David R <99555747+GitExcited@users.noreply.github.com>
2025-12-05 13:34:06 -0500

    day 4- Star 1 In progress- David

--- src/day_04.py
+++ src/day_04.py
@@ -1,49 +1,38 @@
 from pathlib import Path
 
 INPUT_PATH  = Path("../inputs/day_04.txt")
-TOTAL_LINES = 137 # From the .txt file, could've also written code to count them
+TOTAL_LINES = 139 # From the .txt file, could've also written code to count them
 with open(INPUT_PATH) as file:
+    grid = [[el for el in line.rstrip()] for line in file]
+    blank = ['.' for i in range(len(grid[0])) ]
+    grid.insert(0,blank) # First line is blanks
+    grid.append(blank)#Laast linke is blanks
+    #Put blanks at start and end of each line
+    for line in grid:
+        line.insert(0,'.')
+        line.append('.')
+    #Basically transforming the original grid with a padding of '.' all around
 
-    grid = [[el for el in line] for line in file]
-
-
-
-for i in range(0,TOTAL_LINES):
-    for j in range(0,len(grid[i])):
+sum = 0
+for i in range(1,TOTAL_LINES-1):
+    print(f"i{i}")
+    for j in range(1,len(grid[i])-1):
+        print(f"j{j}")
+        current= grid[i][j]
+        if current!= '@': continue
         count = 0 #Number of rolls around
         top = i-1
         bottom = i+1
         left = j-1
         right = j+1
-        set_combinaisons = {}
-        if i>0:
-                #don't check top
-        if j==0:
-            #don't check left
-        if i== TOTAL_LINES-1:
-            #don't check bottom
-        if j== len(grid[i])-1
-            #don't check right
+        coordinates_around = [grid[top][left], grid[top][j], grid[top][right],
+                              grid[i][left],              grid[i][right],
+                              grid[bottom][left],grid[bottom][j], grid[bottom][right]]
 
-        # top
-        if i>0:
-            if grid[i-1][j] == '@':count+=1
+        print(coordinates_around)
+        for coordinate in coordinates_around:
+            if coordinate =='@':count+=1
+        if count<4:
+            sum +=1
 
-        # top right
-            if j< len(grid[i])-1:
-                if grid[i-1][j+1] == '@':count+=1
-        # top left
-            if j> 0:
-                if grid[i - 1][j - 1] == '@': count += 1
-
-        # right
-        if j< len(grid[i])-1:
-            if grid[i][j+1]== '@':count+=1
-        # left
-        if j> 0:
-            if grid[i][j-1]== '@':count+=1
-
-        # bottom
-        # bottom right
-        # bottom left
-print(grid[0])
+print(sum)
