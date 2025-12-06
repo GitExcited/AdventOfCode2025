c322ccf4de279bfd9b34c68d0b0b6d996b4241fb
David R <99555747+GitExcited@users.noreply.github.com>
2025-12-06 17:55:28 -0500

    day 5-  DONE start 1 and 2- David

--- src/day_05.py
+++ src/day_05.py
@@ -3,31 +3,50 @@ from  pathlib import Path
 INPUT_PATH = Path("../inputs/day_05.txt")
 #Theres 192 inputs of ranges
 #from 194 to end its the actual digits
+INPUT_LENGTH = 192
 ranges= []
 digits = []
 with open(INPUT_PATH)  as file:
     i = 0
     for line in file:
-        if i<192:
+        if i<INPUT_LENGTH:
             ranges.append(line.rstrip())
 
-        elif i == 192:
+        elif i == INPUT_LENGTH:
             pass
         else:
             digits.append(line.rstrip())
         i += 1
 
+# PART I
+# count = 0
+# for value in digits:
+#     for bound in ranges:
+#         bar = bound.find('-')
+#         start = int(bound[:bar])
+#         end = int(bound[bar+1:])
+#         if start <= int(value) <= end:
+#             count+=1
+#             break
 
-valid_digits_dict = set()
-for bound in ranges:
-    bar = bound.find('-')
-    start = int(bound[:bar])
-    end = int(bound[bar+1:])
-    for i in range (start,end+1):
-        valid_digits_dict.add(i)
+#PART II
 count = 0
-for value in digits:
-    if value in valid_digits_dict:
-        count+=1
+def get_start(bound:str)->int:
+    return int(bound[:bound.find('-')])
+def get_end(bound:str)->int:
+    return int(bound[bound.find('-')+1:])
+
+#Sort ranges first
+ranges.sort(key=get_start)
+previous_end = 0
+for r in ranges:
+    start = get_start(r)
+    end = get_end(r)
+    if previous_end >= end:
+        continue
+    if previous_end >= start:
+        start = previous_end+1
+    count += end-start+1
+    previous_end = end
 
 print(count)
