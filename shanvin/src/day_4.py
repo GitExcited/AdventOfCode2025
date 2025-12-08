12025f098d575d97701d3f5f969693d4afafb3ee
Shanvin Luo <shanvin.luo@ericsson.com>
2025-12-08 14:51:00 -0500

    D4P2

--- shanvin/src/day_4.py
+++ shanvin/src/day_4.py
@@ -23,6 +23,31 @@ def find_accessible_toilet_paper(grid):
     return result
 
 
+def find_accessible_toilet_paper_P2(grid):
+    rows=(len(grid))
+    col=(len(grid[0]))
+    result=0
+    directions_check=[(0,1), (0,-1), (1, 0), (1, -1), (1,1), (-1, 0), (-1, -1), (-1, 1)]
+    changes=float("inf")
+    while (changes!=0):
+        changes=0
+        for i in range(rows):
+            for j in range(col):
+                roll_counter=0
+                if grid[i][j]=="@":
+                    for r, c in directions_check:
+                        next_r, next_c=r+i, j+c
+                        if (next_r<0 or next_r>=rows or next_c<0 or next_c>=col):
+                            continue
+                        elif(grid[next_r][next_c]=="@"):
+                            roll_counter+=1
+                        if roll_counter>=4:
+                            break
+                    if (roll_counter<4):
+                        grid[i][j]="."
+                        result+=1
+                        changes+=1
+    return result
 
 
 def main():
@@ -32,7 +57,7 @@ def main():
             data=line.strip()
             row=list(data)
             grid.append(row)
-        print(find_accessible_toilet_paper(grid))
+        print(find_accessible_toilet_paper_P2(grid))
 
         
 
