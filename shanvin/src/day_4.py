d7760c2a330cf46ef409b6251fe3857879ec29b8
Shanvin Luo <shanvin.luo@ericsson.com>
2025-12-08 14:43:26 -0500

    Day4P2

---
+++ shanvin/src/day_4.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+INPUT_PATH = Path("../../inputs_shanvin/day_04.txt")
+
+def find_accessible_toilet_paper(grid):
+    rows=(len(grid))
+    col=(len(grid[0]))
+    result=0
+    directions_check=[(0,1), (0,-1), (1, 0), (1, -1), (1,1), (-1, 0), (-1, -1), (-1, 1)]
+    for i in range(rows):
+        for j in range(col):
+            roll_counter=0
+            if grid[i][j]=="@":
+                for r, c in directions_check:
+                    next_r, next_c=r+i, j+c
+                    if (next_r<0 or next_r>=rows or next_c<0 or next_c>=col):
+                        continue
+                    elif(grid[next_r][next_c]=="@"):
+                        roll_counter+=1
+                    if roll_counter>=4:
+                        break
+                if (roll_counter<4):
+                    result+=1
+    return result
+
+
+
+
+def main():
+    grid=[]
+    with open(INPUT_PATH, "r") as f:
+        for line in f:
+            data=line.strip()
+            row=list(data)
+            grid.append(row)
+        print(find_accessible_toilet_paper(grid))
+
+        
+
+if __name__=="__main__":
+    main()
