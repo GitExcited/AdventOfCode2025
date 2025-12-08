bf167a7d1b4e5269d064502d4c468350dad38bec
Shanvin Luo <shanvin.luo@ericsson.com>
2025-12-08 14:23:23 -0500

    Day3 P2

--- shanvin/src/day_3.py
+++ shanvin/src/day_3.py
@@ -19,14 +19,34 @@ def find_largest_number(numbers):
     return (int(str(largest)+str(second_char)))
 
 
+def find_largest_number_12(numbers):
+    largest=0
+    largest_index=0
+    searching_end=11
+    starting_index=-1
+    result=""
+    for i in range(searching_end, -1, -1):
+        print("range:" +str(starting_index+1)+ " " + str(len(numbers)-i))
+        for char in range(starting_index+1, len(numbers)-i):
+            int_number=int(numbers[char])
+            if largest<int_number:
+                largest=int_number
+                largest_index=char
+        result+=str(largest)
+        largest=0
+        starting_index=largest_index
+    return int(result)
+    
+    
+
 
 def main():
     total_count=0
     with open(INPUT_PATH, "r") as f:
         for line in f:
             data=line.strip()
-            result_line=find_largest_number(data)
-            print (result_line)
+            result_line=find_largest_number_12(data)
+            print(result_line)
             total_count+=result_line
     print (total_count)
         
